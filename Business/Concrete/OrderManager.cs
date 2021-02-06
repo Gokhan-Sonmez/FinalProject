6a045d9e4abfcc0d2f0806e34aa68690be0ef41c
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-02-06 23:24:22 +0300

    06.02

---
+++ Business/Concrete/OrderManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public List<Order> GetAll()
+        {
+            return _orderDal.GetAll();
+        }
+
+        public Order GetByID(int orderId)
+        {
+            return _orderDal.Get(o=>o.OrderId==orderId);
+        }
+    }
+}
