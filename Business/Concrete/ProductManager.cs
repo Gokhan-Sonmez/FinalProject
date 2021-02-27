5d8efc3991416e85884a5a4208fceef7c49a1edd
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-02-28 01:34:35 +0300

    27.02

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -28,7 +29,8 @@ namespace Business.Concrete
             _categoryService = categoryService;
 
         }
-
+        
+        [SecuredOperation("admin,product.add")]
     [ValidationAspect(typeof(ProductValidator))]
 
         public IResult Add(Product product)
@@ -56,10 +58,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 1)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 1)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
         }
 
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "10 dan fazla ürün eklenemez";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldıgından eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        internal static string AccessTokenCreated;
+        internal static string UserAlreadyExists;
+        internal static string SuccessfulLogin;
+        internal static User PasswordError;
+        internal static User UserNotFound;
+        internal static string UserRegistered;
     }
 }
