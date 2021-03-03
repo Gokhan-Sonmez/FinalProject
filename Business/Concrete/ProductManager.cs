67674ff1990c7c9539b4dc835e2d8901e7ea04ce
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-03-04 00:15:51 +0300

    03.03

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,8 @@ using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -31,12 +33,12 @@ namespace Business.Concrete
         }
         
         [SecuredOperation("admin,product.add")]
-    [ValidationAspect(typeof(ProductValidator))]
-
+        [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
-               CheckIfProductCountCategoryCorrect(product.CategoryId),
+               CheckIfProductCountOfCategoryCorrect(product.CategoryId),
                CheckIfCategoryLimitExceded());
 
             if (result!=null)
@@ -50,12 +52,14 @@ namespace Business.Concrete
         }
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             _productDal.Update(product);
             return new SuccessResult(Messages.ProductUpdated);
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             //if (DateTime.Now.Hour == 1)
@@ -70,6 +74,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -86,11 +91,11 @@ namespace Business.Concrete
         }
 
         //----BusinesCode----
-        private IResult CheckIfProductCountCategoryCorrect(int categoryId)
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
         {
-
+            //Select count(*) from products where categoryId=1
             var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
-            if (result > 10)
+            if (result >= 15)
             {
                 return new ErrorResult(Messages.ProductCountOfCategoryError);
             }
@@ -100,23 +105,28 @@ namespace Business.Concrete
         private IResult CheckIfProductNameExists(string productName)
         {
             var result = _productDal.GetAll(p => p.ProductName == productName).Any();
-
             if (result)
             {
                 return new ErrorResult(Messages.ProductNameAlreadyExists);
             }
             return new SuccessResult();
         }
-       
+
         private IResult CheckIfCategoryLimitExceded()
         {
             var result = _categoryService.GetAll();
-            if (result.Data.Count<15)
+            if (result.Data.Count > 15)
             {
                 return new ErrorResult(Messages.CategoryLimitExceded);
             }
 
             return new SuccessResult();
+        }
+
+        //[TransactionScopeAspect]
+        public IResult AddTransactional(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,23 @@
+﻿using Core.CrossCuttingConcerns.Cashing;
+using Core.CrossCuttingConcerns.Cashing.Microsoft;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection servicecollection)
+        {
+            servicecollection.AddMemoryCache();
+            servicecollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            servicecollection.AddSingleton<ICacheManager, MemoryCashManager>();
+            servicecollection.AddSingleton<Stopwatch>();
+        }
+    }
+}
